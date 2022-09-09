2874f1f9d38b910f2b9b78422dd440f43bf0b549
SantiRojas <104436569+SantiRojas02@users.noreply.github.com>
2022-09-08 22:43:55 -0300

    Cotizacion_terreno.cs

---
+++ Cotizacion_terreno.cs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace Cotizacion_terreno.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int operacion;
+
+            Console.Write("Ingrese el ancho del terreno: ");
+            int ancho = Convert.ToInt32(Console.ReadLine());
+                
+            Console.Write("Ingrese la longitud del terreno: ");
+            int Longitud = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese el costo del metro cuadrado del terreno: ");
+            int costo = Convert.ToInt32(Console.ReadLine());
+
+            operacion = ancho * Longitud * costo;
+
+            Console.WriteLine("El valor del terreno es: " + operacion);
+
+            
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
